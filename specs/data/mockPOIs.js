21be7bcc04572837a4a39e9ca60ba2d1fd7b47a0
Mictian <mictian@live.com>
2015-11-10 10:46:56 -0300

    Se agregan datos de prueba

--- specs/data/mockPOIs.js
+++ specs/data/mockPOIs.js
@@ -4,5 +4,7 @@ module.exports = [
 	poi('Alsina', -34.611510, -58.393490, 1),
 	poi('Obelisco', -34.604092, -58.381431, 1),
 	poi('Plaza Once', -34.609991, -58.407952, 2),
-	poi('Puerto Madero', -34.609532, -58.366539, 2)
+	poi('Puerto Madero', -34.609532, -58.366539, 2),
+	poi('Antares', -34.613823, -58.373513, 1),
+	poi('Plaza San Martin', -34.594784, -58.375316, 2)
 ]
